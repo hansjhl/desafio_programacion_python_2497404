486cc94757a6669733f9e43bd1d83296bcf4e779
hansjhl <bkzkylehl@gmail.com>
2025-09-05 16:01:02 -0500

    Update Resolucion_01.py

--- 01_desafios_texto/Resolucion_01.py
+++ 01_desafios_texto/Resolucion_01.py
@@ -1,1 +1,6 @@
-
+def palindromo(texto):
+  texto_minuscula = texto.lower()
+  texto_sin_espacios = texto_minuscula.replace(" ","")
+  newTexto = texto_sin_espacios[::-1]
+  return texto_sin_espacios == newTexto
+print (palindromo("hola"))
